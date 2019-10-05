0d7a00c8425e3c0027b132c069b6485e70451f4e
Chris Odom <chrisodom111@gmail.com>
2019-10-05 02:09:10 -0500

    added controllers and views for tasks.

--- CollabFast/Models/TaskList.cs
+++ CollabFast/Models/TaskList.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace CollabFast.Models
 {
     public class TaskList
     {
+        public int ID { get; set; }
         public string taskListName { get; set; }
-        public ICollection<TaskList> tasks { get; set; }
+        public ICollection<ListTask> tasks { get; set; }
     }
 }
