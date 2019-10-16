59fd2bc745f7ea882578a2478bcfb1412e85d9d6
nex1011 <ja112@students.uwf.edu>
2019-10-16 17:08:21 -0500

    database

---
+++ CollabFast/Models/ProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace CollabFast.Models
+{
+    public class ProjectContext : DbContext
+    {
+    }
+
+    public DbSet<Project> Projects { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
+}
