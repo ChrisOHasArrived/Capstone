9d19ca2b5ff73df283cbc9f35b541ee8dfc2a113
Chris Odom <chrisodom111@gmail.com>
2019-10-20 16:09:38 -0500

    ability to create a project

---
+++ CollabFast/Models/CollabFastDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CollabFast.Models
+{
+    public class CollabFastDbContext : DbContext
+    {
+        // Just grabbing all the tables here. It's nasty, but it'll work.
+        public DbSet<Project> Projects { get; set; }
+    }
+}
